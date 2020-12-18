322e3b5faee7aab78d13f768f9fc2920abc4e3f2
Santiago Restrepo <santirstrp@gmail.com>
2020-12-17 19:09:20 -0500

    Terminado el carousel responsive, falta poner fotos, precios, nombres y agregar efecto reverso

--- js/carousel-mobile.js
+++ js/carousel-mobile.js
@@ -30,6 +30,7 @@ window.addEventListener('load', function(){
 	new Glider(document.querySelector('.carousel__lista'), {
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		draggable: true,
 		dots: '.carousel__indicadores',
 		arrows: {
 			prev: '.carousel__anterior',
@@ -38,7 +39,7 @@ window.addEventListener('load', function(){
 		responsive: [
 			{
 			  // screens greater than >= 775px
-			  breakpoint: 450,
+			  breakpoint: 700,
 			  settings: {
 				// Set to `auto` and provide item width to adjust to viewport
 				slidesToShow: 2,
@@ -46,10 +47,10 @@ window.addEventListener('load', function(){
 			  }
 			},{
 			  // screens greater than >= 1024px
-			  breakpoint: 800,
+			  breakpoint: 1024,
 			  settings: {
-				slidesToShow: 4,
-				slidesToScroll: 4
+				slidesToShow: 3,
+				slidesToScroll: 3
 			  }
 			}
 		]
