13fd3b9ffc34718b6ed068b879ce328acbacb8b1
Santiago Restrepo <santirstrp@gmail.com>
2021-08-12 19:27:36 -0500

    Info a script

--- js/greazy-analytics.js
+++ js/greazy-analytics.js
@@ -1,4 +1,18 @@
-const database = firebase.database();
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAW7WI2yBZ2E7rsdTj_KWOXCP9aCstzKCU",
+    authDomain: "analitics-greazy.firebaseapp.com",
+    projectId: "analitics-greazy",
+    storageBucket: "analitics-greazy.appspot.com",
+    messagingSenderId: "885043834152",
+    appId: "1:885043834152:web:22d92e14cd75640280341d",
+    measurementId: "G-2N3SV5F150"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();const database = firebase.database();
 const today = new Date();
 const userDate = `${today.getDate()}-${today.getMonth()+1}-${today.getFullYear()}`;
 const userHour = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
